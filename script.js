c6cb2ef84c145dd312fb8fa959d778a37329b4c6
George S <reneehe@George-Sem-2.local>
2023-05-21 20:53:03 -0700

    pass variables to localstorage

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ $(function () {
   $('.saveBtn').on('click', function () {
     var timeBlockId = $(this).closest('.time-block').attr('id');
     var userInput = $(this).siblings('.description').val();
+    localStorage.setItem(timeBlockId, userInput);
   });
 
 
